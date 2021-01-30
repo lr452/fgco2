e063a2d99d6b8d4415a2c4f70604ed494eec4fd3
Lily Roberts <lr452@mail.example.com>
2021-01-30 15:59:13 +0000

    color map

--- fgco2_shape2.py
+++ fgco2_shape2.py
@@ -3,10 +3,17 @@ import numpy as np
 import iris.quickplot as qplt
 import matplotlib.pyplot as plt
 from iris.coord_categorisation import *
+import matplotlib as mpl
+#from matplotlib import cm
+#from colorspacious import *
+#from collections import *
 
-cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_GFDL-CM4_historical_r1i1p1f1_gr_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
 
-#print(cube)
+
+
+cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_BCC-ESM1_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
+
+print(cube)
 print(cube.shape)
 
 add_month_number(cube, 'time', name='month_number')
@@ -23,6 +30,7 @@ cube4 = cube3.collapsed('time',iris.analysis.MEAN)
 
 
 qplt.pcolormesh(cube4)
+cmaps=[('Diverging', ['PRGn'])]
 #plt.xlim(40,120)
 #plt.ylim()
 plt.show()
