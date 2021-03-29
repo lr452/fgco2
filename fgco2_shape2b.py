83dfc597255b9fdb46e2a1ee16567ddf404f5701
Lily Roberts <lr452@mail.example.com>
2021-03-29 17:54:17 +0100

    fgco2 flux figure

--- fgco2_shape2b.py
+++ fgco2_shape2b.py
@@ -10,13 +10,11 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 
-cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
+cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
 
-#print(cube)
-#print(cube.shape)
 
 add_month_number(cube, 'time', name='month_number')
-cube2 = cube[np.where((cube.coord('month_number').points == 6) | (cube.coord('month_number') == 7) | (cube.coord('month_number') == 8))]
+cube2 = cube[np.where((cube.coord('month_number').points == 12) | (cube.coord('month_number') == 1) | (cube.coord('month_number') == 2))]
 
 #then to average this by each year, so that you have the December-Jan for each year add the 'season year', i.e. a number of each 'season'
 add_season_year(cube2, 'time', name='season_year')
@@ -27,8 +25,9 @@ cube3 = cube2.aggregated_by(['season_year'], iris.analysis.MEAN)
 
 cube4 = cube3.collapsed('time',iris.analysis.MEAN)
 
+
 fig = plt.figure(figsize=[10, 10])
-ax1 = fig.add_subplot(1, 1, 1, projection=ccrs.SouthPolarStereo())
+ax1 = fig.add_subplot(1, 1, 1, projection=ccrs.SouthPolarStereo(), frame_on=False)
 
     # Limit the map to -60 degrees latitude and below.
 ax1.set_extent([-180, 180, -90, -40], ccrs.PlateCarree())
@@ -38,22 +37,16 @@ lons = cube4.coord('longitude').points
 lats = cube4.coord('latitude').points
 data = cube4.data
 
-#ax1.pcolormesh(lons, lats, data,
-#                transform=ccrs.PlateCarree(),
-#                cmap='bwr', vmin=-1.5e-9, vmax=9e-10)
-
-ax1.contourf(lons, lats, data,
+l = ax1.pcolormesh(lons, lats, data,
                 transform=ccrs.PlateCarree(),
-                cmap='bwr')
+                cmap='bwr', vmin=-1.5e-09, vmax=1.5e-09)
 
-ax1.add_feature(cfeature.LAND)
+ax1.add_feature(cfeature.LAND, facecolor='gray', edgecolor='dimgray')
 ax1.gridlines()
-ax1.legend()
-
-    # Compute a circle in axes coordinates, which we can use as a boundary
-    # for the map. We can pan/zoom as much as we like - the boundary will be
-    # permanently circular.
+ax1.spines
+ax1.set_title('ACCESS-ESM1-5')
+fig.colorbar(l, label='kg m-2 s-1', orientation='horizontal', extend='both', shrink=0.8)
 
 plt.show()
 
-#savefig('/disk2/lr452/test.png')
+
