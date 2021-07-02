33346828241385fc05a6b891be67fa19da07679e
Lily Roberts <lr452@mail.example.com>
2021-07-02 11:37:34 +0100

    giss

--- fgco2_shape2b.py
+++ fgco2_shape2b.py
@@ -9,9 +9,7 @@ import matplotlib as mpl
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-
-cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
-
+cube = iris.load_cube('/disk2/lr452/Downloads/fgco2_data/fgco2_Omon_GISS-E2-1-G_historical_r101i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','fgco2')
 
 add_month_number(cube, 'time', name='month_number')
 cube2 = cube[np.where((cube.coord('month_number').points == 12) | (cube.coord('month_number') == 1) | (cube.coord('month_number') == 2))]
@@ -22,30 +20,38 @@ add_season_year(cube2, 'time', name='season_year')
 #then average by the season year:
 cube3 = cube2.aggregated_by(['season_year'], iris.analysis.MEAN)
 
-
 cube4 = cube3.collapsed('time',iris.analysis.MEAN)
-
-
-fig = plt.figure(figsize=[10, 10])
-ax1 = fig.add_subplot(1, 1, 1, projection=ccrs.SouthPolarStereo(), frame_on=False)
-
-    # Limit the map to -60 degrees latitude and below.
-ax1.set_extent([-180, 180, -90, -40], ccrs.PlateCarree())
-
 
 lons = cube4.coord('longitude').points
 lats = cube4.coord('latitude').points
 data = cube4.data
 
-l = ax1.pcolormesh(lons, lats, data,
-                transform=ccrs.PlateCarree(),
-                cmap='bwr', vmin=-1.5e-09, vmax=1.5e-09)
+fig = plt.figure(figsize=[10, 10])
+ax1 = fig.add_subplot(1, 1, 1, projection=ccrs.SouthPolarStereo(), frameon=False)
+#ax1 = plt.axes([0,0,1,1], projection=ccrs.SouthPolarStereo(), frameon=False)
+
+# Limit the map to -60 degrees latitude and below.
+ax1.set_extent([-180, 180, -90, -40], ccrs.PlateCarree())
+
+l=ax1.pcolormesh(lons, lats, data,
+    transform=ccrs.PlateCarree(),
+    cmap='bwr', vmin=-2e-09, vmax=2e-09)
 
 ax1.add_feature(cfeature.LAND, facecolor='gray', edgecolor='dimgray')
 ax1.gridlines()
-ax1.spines
-ax1.set_title('ACCESS-ESM1-5')
-fig.colorbar(l, label='kg m-2 s-1', orientation='horizontal', extend='both', shrink=0.8)
+
+
+ax1.set_title('GISS-E2-1-G')
+
+#ax1.spines['top'].set_color('none')
+#ax1.spines['right'].set_color('none')
+#ax1.spines['bottom'].set_color('none')
+#ax1.spines['left'].set_color('none')
+
+cb =fig.colorbar(l, orientation='horizontal', extend='both', shrink=0.8)
+cb.set_label(label='kg m-2 s-1', weight='bold')
+font = {'weight': 'bold'}
+cb.ax.set_xticklabels(["-2.0", "-1.5", "-1.0", "-0.5", "0.0", "0.5", "1.0", "1.5", "2.0"], fontsize=9, weight='bold')
 
 plt.show()
 
